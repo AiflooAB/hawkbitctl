de149da011951c6ff152c04a53942fe7094ddc22
Linus Wallgren <linus@noomi.com>
2019-05-21 16:20:10 +0200

    Use ~/.netrc file for auth if possible
    If the current domain exists in the ~/.netrc file we will use that,
    unless $PASSWORD is set

--- src/config.sh
+++ src/config.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-if [ -z "$PASSWORD" ]; then
-    echo >&2 "Specify the admin password by running the following two commands"
-    echo >&2 "export PASSWORD"
-    # shellcheck disable=SC1117 disable=SC2028
-    echo >&2 "read -s -p $'Enter your password:\n' -r PASSWORD"
-    exit 1
-fi
-
 if [ -z "$API_HOST" ]; then
     echo >&2 "You need to set the API_HOST variable to point to your hawkbit instance:"
     echo >&2 "export API_HOST=https://hawkbit.example.com"
+    exit 1
+fi
+
+if [ -n "$PASSWORD" ]; then
+    # shellcheck disable=SC2034
+    CURL_AUTH_ARGS=(--user "admin:$PASSWORD")
+elif [ -f ~/.netrc ] && grep --quiet "${API_HOST##*://}" ~/.netrc; then
+    # shellcheck disable=SC2034
+    CURL_AUTH_ARGS=(--netrc)
+else
+    echo >&2 "Either use ~/.netrc (read curl(1) --netrc) or specify the admin password by running the following two commands"
+    echo >&2 "export PASSWORD"
+    # shellcheck disable=SC1117 disable=SC2028
+    echo >&2 "read -s -p $'Enter your password:\n' -r PASSWORD"
     exit 1
 fi
 
